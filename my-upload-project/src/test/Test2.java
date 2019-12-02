857ab3f1cdd724b8d570a8b8dab1a3c6f0822502
李浩博 <lhb941217@outlook.com>
2019-12-02 16:56:12 +0800

    将系统升级到V2.0

--- my-upload-project/src/test/Test2.java
+++ my-upload-project/src/test/Test2.java
@@ -6,5 +6,7 @@ package test;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
+		System.out.println("Hello World !");
+		System.out.println("Hello World !");
 	}
 }
---
+++ my-upload-project/src/test/Test3.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * 控制台输出2.0 
+ */
+public class Test3 {
+	public static void main(String[] args) {
+		System.out.println("能让我看看吗?");
+	}
+}
